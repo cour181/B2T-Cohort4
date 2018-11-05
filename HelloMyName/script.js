9ef78ba1dc26921f741dd59564493f7e58bc96ba
“court181” <sammcval@gmail.com>
2018-11-05 13:58:04 -0500

    two home work

---
+++ HelloMyName/script.js
@@ -0,0 +1,10 @@
+//function entName(){
+//	let tName = prompt('Enter name');
+//	console.log(tName);
+//	document.getElementById('tName');
+//}
+function entName() {
+    let nam = prompt('Enter name')
+ //   document.getElementById("myText");
+    document.getElementById("tName").innerHTML = nam;
+}
